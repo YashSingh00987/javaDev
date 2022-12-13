543b48b8c180a9a8238a5eb9fee3095cf47669df
Yashaswi Singh <Yashaswi Singh@Yash-Singh>
2022-12-13 22:45:58 +0530

    commit 1

--- Lamda expression/src/yash/learn/App.java
+++ Lamda expression/src/yash/learn/App.java
@@ -33,7 +33,7 @@ public class App {
 		
 		
 		Collections.sort(list,(Name o1, Name o2)->{
-			if(o1.getName().length()<o2.getName().length()) {
+			if(o1.getName().length()>o2.getName().length()) {
 				return -1;
 			}else if(o1.getName().length()>o2.getName().length()) {
 				return 1;
